eca7e1ef45ba0c32d49e94201c1ed6c39ae86809
Jordi Vila <jordi.vila@gmail.com>
2014-12-11 09:13:06 +0100

    sdfsdf

--- src/public/test/qunit/common.js
+++ src/public/test/qunit/common.js
@@ -1,20 +1,18 @@
 ﻿    
-    var server = {
-        name : window.location.hostname === ''?'localhost':window.location.hostname,//"localhost",
-        port : window.location.hostname === ''?3000:window.location.port, //3000,
-        getBaseAddress: function () {
-            return "http://" + this.name + ":" + this.port;
-        }
-    };
+var server = {
+    name : window.location.hostname === ''?'localhost':window.location.hostname,
+    port : window.location.hostname === ''?3000:window.location.port, 
+    getBaseAddress: function () {
+        return "http://" + this.name + ":" + this.port;
+    }
+};
 
 
-    var getRandomString = function (stringLength) {
-        var s = "";
-        while (s.length < stringLength && stringLength > 0) {
-            var r = Math.random();
-            s += (r < 0.1?Math.floor(r * 100):String.fromCharCode(Math.floor(r * 26) + (r > 0.5?97:65)));
-        }
-        return s;
-    };     
-
-
+var getRandomString = function (stringLength) {
+    var s = "";
+    while (s.length < stringLength && stringLength > 0) {
+        var r = Math.random();
+        s += (r < 0.1?Math.floor(r * 100):String.fromCharCode(Math.floor(r * 26) + (r > 0.5?97:65)));
+    }
+    return s;
+};
