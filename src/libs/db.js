5c396d2f650d5bd1388de549bb4f38c53b57df8b
Jordi Vila <jordi.vila@gmail.com>
2015-02-23 13:01:08 +0100

    Revert "Revert "Test mongodb 400 Bad Request""
    This reverts commit d5e1685a9205905b437c0946d572c59c02a4c7bf.

--- src/libs/db.js
+++ src/libs/db.js
@@ -3,15 +3,36 @@ var log         = require('../libs/log')(module);
 var config      = require('../libs/config');
 
 
-mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI || config.get('mongoose:uri'));
 
-var db = mongoose.connection;
+function dbInit(cb)
+{
+    mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI || config.get('mongoose:uri'));
 
-db.on('error', function (err) {
-    log.error('connection error:', err.message);
-});
-db.once('open', function callback () {
-    log.info("Connected to DB!");
-});
+    //var db = mongoose.connection;
 
-module.exports.mongoose = mongoose;
+    mongoose.connection.on('error', function (err) {
+        log.error('connection error:', err.message);
+    });
+
+    mongoose.connection.once('open', function callback() {
+        log.info("Connected to DB!");
+
+        cb(null, cb(mongoose.connection));
+    });
+
+    // When the connection is disconnected
+    mongoose.connection.on('disconnected', function () {
+        console.log('Mongoose default connection disconnected');
+    });
+
+    // If the Node process ends, close the Mongoose connection
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            console.log('Mongoose default connection disconnected through app termination');
+            process.exit(0);
+        });
+    });
+
+}
+
+module.exports.dbInit = dbInit;
