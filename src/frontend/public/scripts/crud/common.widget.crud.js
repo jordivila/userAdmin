739a4c89c8c2656b579d7ceb27b92eed3068af51
Jordi Vila <jordi.vila@gmail.com>
2015-04-24 15:12:26 +0200

    fix bug

--- src/frontend/public/scripts/crud/common.widget.crud.js
+++ src/frontend/public/scripts/crud/common.widget.crud.js
@@ -1,4 +1,3 @@
-
 jQuery.widget("ui.crud", jQuery.ui.crudBase,
 {
     options: {
@@ -58,10 +57,24 @@ jQuery.widget("ui.crud", jQuery.ui.crudBase,
         var gridButtonsClass = 'ui-{0}Crud-gridButtons'.format(this.widgetName);
         var gridControlClass = 'ui-{0}Crud-gridControl'.format(this.widgetName);
         var formControlClass = 'ui-{0}Crud-form'.format(this.widgetName);
+        var templateGet = function () {
+
+            var template = '<div class="ui-crud-messages ui-state-default ui-helper-hidden"></div>' +
+                            '<div class="{0}"></div>' +
+                            '<div class="{1} ui-ribbonButtons  ui-state-default"></div>' +
+                            '<div class="{2}"></div>' +
+                            '<div class="{3}"></div>';
+
+            return template
+                .format(gridFilterClass,
+                        gridButtonsClass,
+                        gridControlClass,
+                        formControlClass);
+        };
 
         jQuery(this.element)
             .addClass('ui-crud ui-widget-content')
-            .append(this._templateGet())
+            .append(templateGet())
             .find('div.ui-crud-messages:first')
                 .each(function () {
                     self.options.crudHeaderDomId = jQuery(this);
@@ -215,19 +228,6 @@ jQuery.widget("ui.crud", jQuery.ui.crudBase,
         list: 1,
         filter: 2,
         form: 3
-    },
-    _templateGet: function () {
-        var template = '<div class="ui-crud-messages ui-state-default ui-helper-hidden"></div>' +
-                        '<div class="{0}"></div>' +
-                        '<div class="{1} ui-ribbonButtons  ui-state-default"></div>' +
-                        '<div class="{2}"></div>' +
-                        '<div class="{3}"></div>';
-
-        return template
-            .format(gridFilterClass,
-                    gridButtonsClass,
-                    gridControlClass,
-                    formControlClass);
     },
     _actionSet: function (actionSelected) {
 
