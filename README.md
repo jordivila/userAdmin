9a92b1988fff9675b04632a3fb33ca1c5a20d545
Jordi Vila <jordi.vila@gmail.com>
2015-05-07 17:24:22 +0200

    test

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # userAdmin
 
-
+    
 
 [![Build Status](https://travis-ci.org/jordivila/userAdmin.svg?branch=master)](https://travis-ci.org/jordivila/userAdmin)
-
 [![Dependency Status](https://david-dm.org/jordivila/userAdmin.svg)](https://david-dm.org/jordivila/userAdmin)
-
 [![devDependency Status](https://david-dm.org/jordivila/userAdmin/dev-status.svg)](https://david-dm.org/jordivila/userAdmin#info=devDependencies)
-
 [![Code Climate](https://codeclimate.com/github/jordivila/userAdmin/badges/gpa.svg)](https://codeclimate.com/github/jordivila/userAdmin)
+
+Use this as a check list when deploying
+
+
+- [http://seositecheckup.com](http://seositecheckup.com "Seo Check")
+- [https://asafaweb.com/Scan?Url=jordivilanode.azurewebsites.net](https://asafaweb.com/Scan?Url=jordivilanode.azurewebsites.net)
+
+Next things to keep an eye
+
+- [Deploy to Azure button](http://blog.stevenedouard.com/continuous-delivery-azure-websites-atlassian/ "Deploy to Azure button")
--- src/backend/controllers/home.js
+++ src/backend/controllers/home.js
@@ -11,24 +11,66 @@
     var util = require('../libs/commonFunctions');
     var utilsNode = require('util');
     var commonController = require('../controllers/common');
+    var markdown = require("markdown").markdown;
+    var fs = require('fs');
+
+    var readMeMarkDownContent = null;
+
+    function viewIndexModel(cb) {
+
+        if (readMeMarkDownContent === null) {
+            fs.readFile(__dirname + '../../../../README.md', 'utf8', function (err, data) {
+                if (err) {
+                    return cb(err);
+                }
+
+                readMeMarkDownContent = markdown.toHTML(data);
+
+                cb(null, { ReadmeMarkDown: readMeMarkDownContent });
+            });
+        }
+        else {
+            cb(null, { ReadmeMarkDown: readMeMarkDownContent });
+        }
+
+    }
+
 
 
 
     function index(app, req, res, next) {
 
         if (req.myInfo.IsSEORequest) {
-            res.render(req.myInfo.viewPath, req.myInfo);
+
+            viewIndexModel(function (err, result) {
+
+                if (err) {
+                    return next(err);
+                }
+
+                res.render(req.myInfo.viewPath, util.extend(req.myInfo, result));
+            });
+
         }
         else {
+
             res.sendFile(req.myInfo.viewPath, {
                 root: app.get('views')
             });
+
         }
     }
 
     function indexJSON(app, req, res, next) {
 
-        res.json(req.myInfo);
+        viewIndexModel(function (err, result) {
+
+            if (err) {
+                return next(err);
+            }
+
+            res.json(util.extend(req.myInfo, result));
+        });
     }
 
 })(module);
